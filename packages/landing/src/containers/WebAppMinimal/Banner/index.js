b0f016888a3dd83ebef370b49fb2df0ab1b42630
Carlos <milugarwebcarlos@gmail.com>
2022-08-29 20:01:06 -0500

    cambio al video

--- packages/landing/src/containers/WebAppMinimal/Banner/index.js
+++ packages/landing/src/containers/WebAppMinimal/Banner/index.js
@@ -15,7 +15,7 @@ const Banner = () => {
   return (
     <Section id="home">
       <Pattern />
-      <Container width="1400px">
+      <Container width="1200px">
         <BannerContentWrapper>
           <BannerContent>
             <Heading
@@ -41,7 +41,19 @@ const Banner = () => {
             </div>
           </div> */}
           <Figure className="animate__animated animate__fadeInUp animate__fast">
-            <NextImage />
+            <div className="animate__animated animate__fadeInUp animate__fast">
+            <script src="https://fast.wistia.com/embed/medias/ux6ahuak0p.jsonp" async></script>
+            <script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+            <div className="wistia_responsive_padding" style={{padding: '56.25% 0 0 0', position: 'relative'}}>
+                <div className="wistia_responsive_wrapper" style={{height: '100%', left: 0, position: 'absolute', top: 0,width: '100%'}}>
+                    <div className="wistia_embed wistia_async_ux6ahuak0p videoFoam=true" style={{height: '100%', position: 'relative', width: '100%'}}>
+                      <div className="wistia_swatch" style={{height: '100%', left: 0, opacity: 0, overflow: 'hidden', position: 'absolute', top: 0, transition: 'opacity 200ms', width: '100%'}}>
+                        <img src={`https://fast.wistia.com/embed/medias/ux6ahuak0p/swatch`} style={{ filter: 'blur(5px)', height: '100%', 'object-fit': 'contain', width: '100%'}} alt="" aria-hidden="true" onload="this.parentNode.style.opacity=1;" />
+                      </div>
+                    </div>
+                </div>
+            </div>
+          </div>
           </Figure>
         </BannerContentWrapper>
       </Container>
